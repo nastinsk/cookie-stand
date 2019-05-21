e89ba84776b29ed0d75f0a5993944a26c7cfa2be
nastinsk@gmail.com <nastinsk@gmail.com>
2019-05-21 01:57:52 -0700

    objects added to html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
+//function for random numbers. The maximum is exclusive and the minimum is inclusive
 var randomCustomers = function (minCust, maxCust) {
   var min = Math.ceil(minCust);
   var max = Math.floor(maxCust);
   var randomResult = Math.floor(Math.random() * (max-min)) + min;
-  return Math.floor(Math.random() * (max-min)) + min;//The maximum is exclusive and the minimum is inclusive
-
-}
+  return Math.floor(Math.random() * (max-min)) + min;
+};
 
 var pike = {
   name: '1st and Pike',
@@ -14,75 +14,134 @@ var pike = {
   maxCust:65,
   avgSale:6.3, //avrage cookie sale for 1 customer
   amountOfCust: randomCustomers,
-  cookiesInHour: []
+  cookiesInHour: [],
 };
+
 //check how random function works with array
 console.log(pike.amountOfCust(pike.minCust, pike.maxCust));
-
 
 var seaTac = {
   name: 'SeaTac Airport',
   minCust:3,
   maxCust:24,
-  AvgSale:1.2,
+  avgSale:1.2,
   amountOfCust: randomCustomers,
   cookiesInHour: []
 };
-console.log(seaTac.amountOfCust(seaTac.minCust, seaTac.maxCust));
 
 var seattleCenter = {
   name: 'Seattle Center',
   minCust:11,
   maxCust:38,
-  AvgSale:3.7,
+  avgSale:3.7,
   amountOfCust: randomCustomers,
   cookiesInHour: []
+  
 
 };
-console.log(seattleCenter.amountOfCust(seattleCenter.minCust, seattleCenter.maxCust));
 
 var capitolHill= {
   name: 'Capitol Hill',
   minCust:20,
   maxCust:38,
-  AvgSale:2.3,
+  avgSale:2.3,
   amountOfCust: randomCustomers,
   cookiesInHour: []
+  
 };
-console.log(capitolHill.amountOfCust(capitolHill.minCust, capitolHill.maxCust));
 
 var alki = {
   name: 'Alki',
   minCust:2,
   maxCust:16,
-  AvgSale:4.6,
+  avgSale:4.6,
   amountOfCust: randomCustomers,
   cookiesInHour: []
 };
-console.log(alki.amountOfCust(alki.minCust, alki.maxCust));
+
+//array of the stores objects
+var stores = [pike, seaTac, seattleCenter, capitolHill, alki];
+
+// var connectHtml = function() {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = pike.name;
+//   storesList.appendChild(liEl);
+// };
+// connectHtml();
 
 
+var total;
+//function to count random amount of cookies for every hour and total amount
+var randomCookies = function (stores){
+  var j =6;
+  total = 0;
+  for (var i = 0; i < 15; i++) {
 
-var j =6;
-for (var i = 0; i < 15; i++) {
-  //for 6am to 11am included
-  if (i < 6) {
-    var cookiesInHourAvg = pike.amountOfCust(pike.minCust, pike.maxCust)*Math.ceil(pike.avgSale);//random number of cookies for every hour
-    pike.cookiesInHour[i] = `${j}am: ${cookiesInHourAvg}`;
-    j++;
-  //for 12pm
-  } else if (i === 6) {
-      cookiesInHourAvg = pike.amountOfCust(pike.minCust, pike.maxCust)*Math.ceil(pike.avgSale);//random number of cookies for every hour
-      pike.cookiesInHour[i] = `12pm: ${cookiesInHourAvg}`;
-  //for 1pm to 8pm
-  } else {
-      cookiesInHourAvg = pike.amountOfCust(pike.minCust, pike.maxCust)*Math.ceil(pike.avgSale);//random number of cookies for every hour
-      pike.cookiesInHour[i] = `${i-6}pm: ${cookiesInHourAvg}`;
+    //for 6am to 11am included
+    if (i < 6) {
+      var cookiesInHourAvg = stores.amountOfCust(stores.minCust, stores.maxCust)*Math.ceil(stores.avgSale);//random number of cookies for every hour
+      total+= cookiesInHourAvg;
+      stores.cookiesInHour[i] = `${j}am: ${cookiesInHourAvg} cookies`;
       j++;
+      
+    //for 12pm
+    } else if (i === 6) {
+      cookiesInHourAvg = stores.amountOfCust(stores.minCust, stores.maxCust)*Math.ceil(stores.avgSale);//random number of cookies for every hour
+      total+= cookiesInHourAvg;
+      stores.cookiesInHour[i] = `12pm: ${cookiesInHourAvg} cookies`;
+            
+    //for 1pm to 8pm included
+    } else {
+      cookiesInHourAvg = stores.amountOfCust(stores.minCust, stores.maxCust)*Math.ceil(stores.avgSale);//random number of cookies for every hour
+      total+= cookiesInHourAvg;
+      stores.cookiesInHour[i] = `${i-6}pm: ${cookiesInHourAvg} cookies`;
+      j++;
+    }
+  } 
+  stores.total = total;
+ 
+};
+
+
+
+//connecting with html with <ul #stores>
+var storesList = document.getElementById('stores');
+
+//loop to use function randomCookies in all elements of the 'stores' array 
+for (var i = 0; i < stores.length; i++){
+  randomCookies(stores[i]);
+  // stores[i].total = total;
+  console.log(stores[i].name);
+  console.log (stores[i].cookiesInHour);
+  console.log(`Store total:${stores[i].total}`);
+  
+
+
+//using the same loop to make a list with future hyperlinks of all the object.names on top of the html
+  var liEl = document.createElement('li');
+  liEl.textContent = `${stores[i].name}`;
+  storesList.appendChild(liEl);
+}
+
+var storesList = document.getElementsByClassName('location');
+//loop to show the random cookies result for each hour for each location in html
+for (i = 0; i < stores.length; i++){
+  var lhEl = document.createElement('lh');
+  lhEl.textContent = `${stores[i].name} location`;
+  storesList[i].appendChild(lhEl);
+  
+  for (var j=0; j < stores[i].cookiesInHour.length; j++){
+    liEl = document.createElement('li');
+    liEl.textContent = stores[i].cookiesInHour[j];
+    storesList[i].appendChild(liEl);
   }
 }
 
-//check if array works
-console.log (pike.cookiesInHour);
-
-
+// //sum of array
+// var total = 0;
+// for (var i = 0; i < pike.cookiesInHour.length; i++) {
+//   total += stores[0].cookiesInHour;
+//   stores[0].total = total;
+  
+// }
+// console.log(stores[0].total); 
