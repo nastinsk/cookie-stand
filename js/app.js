ce5edda20756d53d39dcff7433c1c302892ac98a
nastinsk@gmail.com <nastinsk@gmail.com>
2019-05-23 20:56:15 -0700

    data added to table, totals added

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ function Store (name,minCustInHour, maxCustInHour, avgCookieSaleInHour ) {
   this.minCustInHour = minCustInHour;
   this.maxCustInHour = maxCustInHour;
   this.avgCookieSaleInHour = avgCookieSaleInHour;
-  // this.amountOfCustomersEachHour = ; //method we will get using random function
   this.cookiesEachHour = [];
   this.totalCookiesOfTheDay;
   stores.push(this);
@@ -29,31 +28,109 @@ Store.prototype.calkCookiesEachHour = function (){
   this.totalCookiesOfTheDay = total;
 };
 
+//head of table
+var tableHeadHorizontal = document.getElementById('tableHeadHorizontal');
+for (var i = 0; i < workHours.length; i++){
+  var thEl = document.createElement('th');
+  thEl.textContent = workHours[i];
+  tableHeadHorizontal.appendChild(thEl);
+}
+thEl = document.createElement('th');
+thEl.textContent = 'Total';
+tableHeadHorizontal.appendChild(thEl);
+
+//data rows in tableBody
+Store.prototype.connectHtml = function () {
+   
+  //print store name in 1st column
+  var table = document.getElementById('tableBody');
+  var trEl = document.createElement('tr');
+  table.appendChild(trEl);
+  thEl = document.createElement('th');  //store name in table
+  thEl.textContent = this.name;
+  trEl.appendChild(thEl);
+
+  //print table data
+  for (i = 0; i < this.cookiesEachHour.length; i++){
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesEachHour[i];
+    trEl.appendChild(tdEl);
+  }
+  //print cookies total for the day
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.totalCookiesOfTheDay;
+  trEl.appendChild(tdEl);
+};
+
+
 var pike = new Store('1st and Pike', 23, 65, 6.3);
 pike.randomCust();
 pike.calkCookiesEachHour();
+pike.connectHtml();
 console.log(stores[0]);
 
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
 seaTac.randomCust();
 seaTac.calkCookiesEachHour();
+seaTac.connectHtml();
 console.log(stores[1]);
 
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 seattleCenter.randomCust();
 seattleCenter.calkCookiesEachHour();
+seattleCenter.connectHtml();
 console.log(stores[2]);
 
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 capitolHill.randomCust();
 capitolHill.calkCookiesEachHour();
+capitolHill.connectHtml();
 console.log(stores[3]);
 
 var alki = new Store('Alki', 2, 16, 4.6);
 alki.randomCust();
 alki.calkCookiesEachHour();
+alki.connectHtml();
 console.log(stores[4]);
 
+//total for the each hour in all stores
+var sumByHourArray = [];
+for(var j=0; j<stores[0].cookiesEachHour.length; j++){
+  var sumByHour=0;
+  for( i = 0; i<stores.length; i++){
+    sumByHour+= stores[i].cookiesEachHour[j];
+    
+  }
+  sumByHourArray.push(sumByHour);
+}
+console.log(sumByHourArray);
+
+//GRAND TOTAL SUM
+var grandTotal = 0;
+for(i=0; i<stores.length; i++){
+  grandTotal+= stores[i].totalCookiesOfTheDay;
+}
+console.log(grandTotal);
+
+
+//connect sum by hour array and html table
+var tableFoot = document.getElementById('tableFoot');
+var trEl = document.createElement('tr');
+tableFoot.appendChild(trEl);
+thEl = document.createElement('th');  //store name in table
+thEl.textContent = 'Total';
+trEl.appendChild(thEl);
+
+//print in table hours total
+for (i = 0; i < sumByHourArray.length; i++){
+  var tdEl = document.createElement('td');
+  tdEl.textContent = sumByHourArray[i];
+  trEl.appendChild(tdEl);
+}
+tdEl = document.createElement('td');
+tdEl.textContent = grandTotal;
+trEl.appendChild(tdEl);
+ 
 
 
 
